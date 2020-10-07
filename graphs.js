5cd34bf96db5836cc64ce6b7edbd0da6ef142353
Nathan Eloe <powerofazure@gmail.com>
2020-10-07 12:44:06 -0500

    Make the graph a "global" variable

--- graphs.js
+++ graphs.js
@@ -1,7 +1,8 @@
 document.getElementById('test').innerHTML='<p>Hello world!</p>'
+let G = new jsnx.DiGraph();
 fetch('./prereqs.json').then(response => response.json())
   .then(data =>  {
-    let G = new jsnx.DiGraph();
+
     G.addNodesFrom(Object.keys(data))
     console.log(Object.keys(data))
     console.log('hello world?')

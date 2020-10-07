ab577178d513f18446287bb6ab0905c80ea6b999
Nathan Eloe <powerofazure@gmail.com>
2020-10-07 12:41:50 -0500

    Try to start building networkx graph

--- graphs.js
+++ graphs.js
@@ -1,3 +1,7 @@
 document.getElementById('test').innerHTML='<p>Hello world!</p>'
 fetch('./prereqs.json').then(response => response.json())
-  .then(data => console.log(data));
+  .then(data =>  {
+    let G = new jsnx.DiGraph();
+    G.addNodesFrom(Object.keys(data))
+    console.log(Object.keys(data))
+  });
